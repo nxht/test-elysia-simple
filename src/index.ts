62988cf6925cf7a4d77def9401b288cdb20b486c
hgpark <hanguyl91@gmail.com>
2024-02-06 17:45:15 +0900

    test elysia swagger error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia({
+  cookie: {
+    secrets: "test",
+    sign: ["test"],
+  },
+})
+  .use(swagger())
+  .get("/", () => "hi", {
+    response: t.String({ description: "sample description" }),
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
