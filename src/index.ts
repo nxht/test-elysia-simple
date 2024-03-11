e1df0a843dfd426e93179b7b3d7a90b94bea5ba3
hgpark <hanguyl91@gmail.com>
2024-03-12 01:21:11 +0900

    feat: add dprint

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,9 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
 
-
-class AClass {
-  run() {
-    return 1
-  }
-}
-
-
-const p = new Elysia().decorate('a', new AClass()).resolve(({a}) => {
-  const b = a.run()
-  return {b}
-})
-
-
-const app = new Elysia().use(p)
-  .get("/", ({b}) => `hi, ${b}`)
+const app = new Elysia()
+  .get('/', () => `hello world`)
   .listen(3000);
 
 console.log(
-  `http://${app.server?.hostname}:${app.server?.port}`
+  `http://${app.server?.hostname}:${app.server?.port}`,
 );
